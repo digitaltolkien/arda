033a29a67d260790eb4cae06e6dbe04feb458b39
James Tauber <jtauber@jtauber.com>
2022-05-25 23:06:09 -0400

    python-publish github action

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="arda",
-    version="0.1",
+    version="0.1.0",
     description="Python library for Tolkien-related data processing",
     url="http://github.com/digitaltolkien/arda",
     author="James Tauber",
