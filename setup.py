6dbaeb12a023fd5e861e9de16d19062ea1eecb95
James Tauber <jtauber@jtauber.com>
2022-06-04 22:34:22 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="arda",
-    version="0.1.0",
+    version="0.2.0",
     description="Python library for Tolkien-related data processing",
     long_description=long_description,
     long_description_content_type="text/markdown",
